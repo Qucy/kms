fbc228d0712fef0991d9daca1ccdb9bf215d500d
Nicholas Cheung <nicholascheung25.gtd@gmail.com>
2022-08-04 11:08:18 +0800

    fetch all tags onmount after login

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setAllTagList } from '../hooks/tag/tagSlice';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import Header from './main/header'
-import Menu from './main/menu'
-import Content from './main/content'
+import Header from './main/header';
+import Menu from './main/menu';
+import Content from './main/content';
+
+import { API_TAG } from '../utils/api';
 
 const mdTheme = createTheme();
 
@@ -17,9 +21,9 @@ function DashboardContent() {
   return (
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: 'flex' }}>
-        <CssBaseline/>
-        <Header open={open} toggleDrawer={toggleDrawer}/>
-        <Menu open={open} toggleDrawer={toggleDrawer}/>
+        <CssBaseline />
+        <Header open={open} toggleDrawer={toggleDrawer} />
+        <Menu open={open} toggleDrawer={toggleDrawer} />
         <Content />
       </Box>
     </ThemeProvider>
@@ -27,5 +31,22 @@ function DashboardContent() {
 }
 
 export default function Dashboard() {
+  const dispatch = useDispatch();
+  //fetch all tags
+  React.useEffect(() => {
+    const fetchAllTags = async () => {
+      try {
+        const response = await API_TAG.getAllTags();
+
+        if (response.status === 200) {
+          dispatch(setAllTagList(response.data));
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchAllTags();
+  }, []);
+
   return <DashboardContent />;
 }
