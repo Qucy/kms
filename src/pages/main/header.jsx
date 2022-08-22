f49fb4586218f093613b61e0b8560729e6e3c643
Alex <alexlocheungyu@gmail.com>
2022-08-22 15:00:45 +0800

    title change

--- src/pages/main/header.jsx
+++ src/pages/main/header.jsx
@@ -55,7 +55,7 @@ const Header = (props) => {
             noWrap
             sx={{ flexGrow: 1 }}
         >
-            Knowledge management system
+            CLCM Campaign Library
         </Typography>
         </Toolbar>
     </AppBar>
